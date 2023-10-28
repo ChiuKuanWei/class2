a47ddd91652161ca8f3f418ac63720d0f34f8b30
ChiuKuanWei <harry20040723@gmail.com>
2023-10-28 15:45:48 +0800

    20231028_1545

--- day3_1.py
+++ day3_1.py
@@ -8,12 +8,11 @@
 
 import random
 min = 1
-max = 10
+max = 100
 count = 0
 target = random.randint(min,max)
 print("============猜數字遊戲=================\n")
 while(True):
-    count += 1
     try:       
         keyin = int(input(f"猜數字的範圍{min}~{max}:"))
     except:
@@ -21,14 +20,17 @@ while(True):
         continue
 
     if(keyin >= min and keyin <= max):
+        count += 1
         if keyin == target:
             print(f"賓果!猜對了, 答案是:{keyin}")
             break
-        else:
-            print("請再努力!!!!")
-            continue
+        elif keyin > target:
+            print("在小一點!")                        
+        elif keyin < target:
+            print("在大一點!")
     else:
         print("請輸入提示範圍內的數字")
+
 print(f"您猜了{count}次")
 print("遊戲結束")
 
