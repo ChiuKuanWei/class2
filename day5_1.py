455ba42a884c9659d1ee49949f08e6c643fea7f9
ChiuKuanWei <harry20040723@gmail.com>
2023-11-18 09:09:45 +0800

    20231118_0909

--- day5_1.py
+++ day5_1.py
@@ -1,4 +1,6 @@
 import numpy as np
+import random
+import csv
 #============================list=======================================
 # #             0        1        2       3       4        5        6
 # weekdays = ["星期一","星期二","星期三","星期四","星期五","星期六","星期七"]
@@ -16,36 +18,63 @@ import numpy as np
 # for day in weekdays:
 #     print(f'week:{day}')
 
-thriller = ['Thriller', 'Billie Jean', 'Beat It']
-thriller.append('That Girl is Mine')
-print(thriller)
-thriller.insert(1,"John Weak")
-print(f'插入了{thriller[1]}:{thriller}')
+# thriller = ['Thriller', 'Billie Jean', 'Beat It']
+# thriller.append('That Girl is Mine')
+# print(thriller)
+# thriller.insert(1,"John Weak")
+# print(f'插入了{thriller[1]}:{thriller}')
 
-bad = ['Bad','Smooth Criminal','Speed Demon','Man in the Mirror','Dirty Diana']
-print(bad)
-popped = bad.pop()
-print(f'{popped}被刪除了')
+# bad = ['Bad','Smooth Criminal','Speed Demon','Man in the Mirror','Dirty Diana']
+# print(bad)
+# popped = bad.pop()
+# print(f'{popped}被刪除了')
 
 
-#把 bad_2 這個 list 加到 bad_1 中整合，要把第二個 list 中的元素 (而不是整個 list) 加到第一個 list 中的做法，就是改成使用 extend
-bad_1 = ['Bad1', 'Smooth Criminal1','Speed Demon1']
-bad_2 = ['Man in the Mirror2', 'Dirty Diana2']
-bad_1.extend(bad_2)
-print(f'bad_2已新增至bad_1:{bad_1}')
+# #把 bad_2 這個 list 加到 bad_1 中整合，要把第二個 list 中的元素 (而不是整個 list) 加到第一個 list 中的做法，就是改成使用 extend
+# bad_1 = ['Bad1', 'Smooth Criminal1','Speed Demon1']
+# bad_2 = ['Man in the Mirror2', 'Dirty Diana2']
+# bad_1.extend(bad_2)
+# print(f'bad_2已新增至bad_1:{bad_1}')
 
-students = []
-stud1 = [10,20,30,40]
-stud2 = [50,60,70,80]
-stud3 = [90,100,110,120]
-students.append(stud1)
-students.append(stud2)
-students.append(stud3)
-arr = np.array(students)
-print(arr)
-print(arr.shape)
+# students = []
+# stud1 = [10,20,30,40]
+# stud2 = [50,60,70,80]
+# stud3 = [90,100,110,120]
+# students.append(stud1)
+# students.append(stud2)
+# students.append(stud3)
+# arr = np.array(students)
+# print(arr)
+# print(arr.shape)
 
+# # 建立一個學生5個分數
+# #建立50個學生
+
+def Createstud() -> list:
+    stud = []
+    for j in range(5):
+        source = random.randint(50,100)
+        stud.append(source)
+    return stud
+
+def CreateClasses(number:int) -> None:
+    student = []
+    for i in range(number):
+        student.append(Createstud()) 
+    arr = np.array(student)
+    print(arr)
+    print(f'長度:{len(arr)}')
+    return arr
+ 
+students = CreateClasses(10)
 #=======================================================================
+
+
+file = open('C:\python-training\class2\students.csv', mode='w', encoding='utf-8' , newline='')
+csv_writer = csv.writer(file)
+csv_writer.writerow(["國文","英文","數學","歷史","地理"])
+csv_writer.writerows(students)
+file.close()  
 
 #============================dictionary=================================
              
---
+++ day5_2.py
@@ -0,0 +1,50 @@
+import csv
+import requests
+import json
+from fake_useragent import UserAgent
+import certifi
+import io
+import pandas as pd
+
+# file = open('個股日成交資訊.csv', mode='r', encoding='utf-8' , newline='')
+# csv_reader = csv.reader(file)
+# data_list = list(csv_reader)
+
+# # 假设第一行包含标头
+# headers = data_list[0]
+
+# # 将数据转换为字典列表
+# json_data = [dict(zip(headers, row)) for row in data_list[1:]]
+
+# # 将字典列表转换为JSON格式的字符串
+# json_string = json.dumps(json_data, indent=2, ensure_ascii=False)
+
+# # 或者，您可以将JSON数据写入文件
+# # with open('output.json', 'w', encoding='utf-8') as json_file:
+# #     json.dump(json_data, json_file, ensure_ascii=False)
+
+# print(json_string)
+
+# 創建UserAgent物件
+ua = UserAgent()
+# 從UserAgent物件中獲取一個隨機的User-Agent，用於發送HTTP請求，降低被網站阻擋或限制的風險
+user_agent = ua.random
+my_head = {'user-agent': user_agent}
+
+#page_url = f'https://service.taipower.com.tw/data/opendata/apply/file/d010004/本公司土地出租作電動機車電池交換站明細表(新).csv'
+page_url = f'https://data.ntpc.gov.tw/api/datasets/010e5b15-3823-4b20-b401-b1cf000550c5/csv/file'
+
+page_response = requests.get(page_url, headers= my_head)
+page_content = page_response.content.decode(encoding='utf-8-sig')
+#print(page_response.text)
+file = io.StringIO(page_content)
+csv_reader = csv.reader(file)
+csv_list = list(csv_reader)
+# for item in csv_list:
+#     print(item)
+
+df = pd.DataFrame(csv_list)
+print(df)
+
+
+
