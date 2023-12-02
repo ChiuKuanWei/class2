6195e95a4255b61960793a9d9866dfe1e53927e7
ChiuKuanWei <harry20040723@gmail.com>
2023-12-02 09:17:44 +0800

    20231202_0917

--- day7_2.py
+++ day7_2.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from openpyxl import load_workbook
 
 scroes = np.random.randint(50, 100,size=(50,5))
 scores_df = pd.DataFrame(scroes,columns=['國文', '英文', '數學', '地理', '歷史'],index=range(1,51))
@@ -7,14 +8,22 @@ scores_df = pd.DataFrame(scroes,columns=['國文', '英文', '數學', '地理',
 
 scores_df['總分'] = scores_df.sum(axis = 1,skipna=True)
 scores_df['平均'] = scores_df.mean(axis = 1,skipna=True)
-print(scores_df)
+# print(scores_df)
+
+scores_df.style\
+    .format(precision=2)\
+    .highlight_between(left=50,right=59,props='color:white; background-color:#CB1B45;')\
+    .highlight_max(axis=1,subset=scores_df.columns[:5],props='color:white; background-color:#2B5F75;')
+
+book = load_workbook('天龍國小.xlsx')
+
 
 # scores_df = scores_df.sort_values(by='總分',ascending=True).iloc[:5]
 # print(scores_df)
 
 #使用subscript[],過濾資料
-scores_df1 = scores_df.query('平均<120 and 歷史<60')
-print(scores_df1)
+# scores_df1 = scores_df.query('平均<120 and 歷史<60')
+# print(scores_df1)
 
 
 
--- day7_3.py
+++ day7_3.py
@@ -5,7 +5,17 @@ read_file = pd.read_csv('world.csv')
 # print(read_file)
 
 taiwan = read_file[read_file['國家'] == '台灣']
-print(taiwan)
+# print(taiwan)
 
 Japan = read_file[read_file['國家'] == '日本']
-print(Japan)
+# print(Japan)
+
+# taiwan = read_file.query('日期>="2020-01-01" and 日期<="2020-01-31"')
+# print(taiwan)
+
+# 新增確診數最多的第一筆(Taiwan)
+Max = taiwan.sort_values(by='新增確診數',ascending=False).iloc[:1]
+print(Max)
+
+Max2 = Japan.query('新增確診數>=50000')
+print(Max2)
